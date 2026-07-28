652757c3539bf7d1f79f2996945aee4414c76ac8
SATHISH S <sathishsjn143@gmail.com>
2026-07-28 16:23:39 +0530

    Fixed service worker install

--- service-worker.js
+++ service-worker.js
@@ -1,59 +1,38 @@
-const CACHE_NAME = "music-player-v5";
+const CACHE_NAME = "music-player-v6";
 
 const CACHE_FILES = [
-
     "./",
     "./index.html",
     "./style.css",
     "./script.js",
-
-    "./images/cover1.jpg",
-    "./images/cover2.jpg",
-
-    "./songs/song1.mp3",
-    "./songs/song2.mp3"
+    "./manifest.json"
 ];
 
 
-
-self.addEventListener("install", (event) => {
-
+self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => {
-
-            return cache.addAll(CACHE_FILES);
-
-        })
-        .catch((error)=>{
-            console.log("Cache error:", error);
-        })
+        .then(cache => cache.addAll(CACHE_FILES))
     );
-
 });
 
 
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        self.clients.claim()
+    );
+});
 
-self.addEventListener("fetch", (event)=>{
+
+self.addEventListener("fetch", event => {
 
     event.respondWith(
-
         caches.match(event.request)
-        .then((response)=>{
+        .then(response => {
 
-            if(response){
-                return response;
-            }
-
-            return fetch(event.request)
-            .catch(()=>{
-
-                return new Response("Offline file not available");
-
-            });
+            return response || fetch(event.request);
 
         })
-
     );
 
 });
