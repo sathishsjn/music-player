554cc5d820b96ae875950b1dd945561d639b55a6
SATHISH S <sathishsjn143@gmail.com>
2026-07-28 16:02:50 +0530

    2nd minor update

--- script.js
+++ script.js
@@ -159,3 +159,15 @@ function createPlaylist() {
 
 
 createPlaylist();
+
+if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker.register("service-worker.js")
+    .then(function(){
+
+        console.log("Offline mode enabled");
+
+    });
+
+}
+
---
+++ service-worker.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "music-player-v4";
+
+const CACHE_FILES = [
+
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+
+    "./images/cover1.jpg",
+    "./images/cover2.jpg",
+
+    "./songs/song1.mp3",
+    "./songs/song2.mp3"
+];
+
+
+
+self.addEventListener("install", (event) => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+
+            return cache.addAll(CACHE_FILES);
+
+        })
+        .catch((error)=>{
+            console.log("Cache error:", error);
+        })
+    );
+
+});
+
+
+
+self.addEventListener("fetch", (event)=>{
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then((response)=>{
+
+            if(response){
+                return response;
+            }
+
+            return fetch(event.request)
+            .catch(()=>{
+
+                return new Response("Offline file not available");
+
+            });
+
+        })
+
+    );
+
+});
