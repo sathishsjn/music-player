957a20c501916e0149d87d8e635068c4ef2df935
SATHISH S <sathishsjn143@gmail.com>
2026-07-29 13:56:00 +0530

    Fixed service worker v9

--- service-worker.js
+++ service-worker.js
@@ -1,76 +1,98 @@
-const CACHE_NAME = "music-player-v8";
+const CACHE_NAME = "music-player-v9";
 
 const CACHE_FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
 
-  "./icons/music-logo.png",
+    "./icons/music-logo.png",
 
-  "./images/cover1.jpg",
-  "./images/cover2.jpg",
+    "./images/cover1.jpg",
+    "./images/cover2.jpg",
 
-  "./songs/song1.mp3",
-  "./songs/song2.mp3",
+    "./songs/song1.mp3",
+    "./songs/song2.mp3"
 ];
 
 // Install
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_FILES)),
-  );
-
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(CACHE_FILES))
+            .then(() => self.skipWaiting())
+    );
 });
 
 // Activate
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        }),
-      );
-    }),
-  );
-
-  self.clients.claim();
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
 });
 
 // Fetch
 self.addEventListener("fetch", (event) => {
-   if (event.request.method !== "GET") return;
 
-    if (event.request.headers.has("range")) return; 
+    // Cache only GET requests
+    if (event.request.method !== "GET") {
+        return;
+    }
 
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
+    // Don't cache audio range requests (206 Partial Content)
+    if (event.request.headers.has("range")) {
+        return;
+    }
 
-      return fetch(event.request)
-        .then((networkResponse) => {
-          const responseClone = networkResponse.clone();
+    event.respondWith(
 
-          if (networkResponse.status === 200) {
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(event.request, responseClone);
-            });
-          }
+        caches.match(event.request).then((cachedResponse) => {
 
-          return networkResponse;
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            return fetch(event.request)
+                .then((networkResponse) => {
+
+                    // Cache only successful (200 OK) responses
+                    if (
+                        networkResponse &&
+                        networkResponse.status === 200 &&
+                        networkResponse.type === "basic"
+                    ) {
+
+                        const responseClone = networkResponse.clone();
+
+                        caches.open(CACHE_NAME).then((cache) => {
+                            cache.put(event.request, responseClone);
+                        });
+
+                    }
+
+                    return networkResponse;
+
+                })
+                .catch(() => {
+
+                    // Offline fallback for HTML pages
+                    if (event.request.destination === "document") {
+                        return caches.match("./index.html");
+                    }
+
+                });
+
         })
-        .catch(() => {
-          if (event.request.destination === "document") {
-            return caches.match("./index.html");
-          }
-        });
-    }),
-  );
+
+    );
+
 });
